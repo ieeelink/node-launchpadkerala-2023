79867da339bc4d5f95b0543eaf8808e606d18da4
Basil Reji <84758142+basil-reji@users.noreply.github.com>
2023-03-20 00:39:37 +0530

    Updated to Launchpad Kerala

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-var title = 'IEEE Job Fair 2022'
+var title = 'Launchpad Kerala'
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
