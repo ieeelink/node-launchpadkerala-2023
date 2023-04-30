971f9bd3136fea48031e7f8facc3c602c146822e
Basil Reji <84758142+basil-reji@users.noreply.github.com>
2023-04-30 22:04:33 +0530

    allotment pages added

--- controller/user.js
+++ controller/user.js
@@ -70,6 +70,54 @@ const candidate = {
                 reject("Please enter a valid Launchpad Kerala ID.")
             }
         })
+    },
+
+    view: (data) => {
+        return new Promise(async (resolve, reject) => {
+
+            if(data.lpk_id.startsWith(" ")){
+                data.lpk_id = data.lpk_id.substring(1);
+            }
+            if(data.lpk_id.endsWith(" ")){
+                data.lpk_id = data.lpk_id.substring(0, 8);
+            }
+            
+            if (String(data.lpk_id.length) == 8 && data.lpk_id.includes("LPK1")) {
+                db.get()
+                    .collection(collections.CANDIDATES)
+                    .findOne({
+                        "id": data.lpk_id
+                    })
+                    .then((response) => {
+                        // console.log(response)
+                        if (response) {
+                            if(data.email.startsWith(" ")){
+                                data.email = data.email.substring(1);
+                            }
+                            if(data.email.endsWith(" ")){
+                                data.email = data.email.substring(0, 8);
+                            }
+                            if(response.email == data.email) {
+                                resolve(response)
+                            }else if(response.other_email.original == data.email){
+                                resolve(response)
+                            }else if(response.other_email.corrected == data.email){
+                                resolve(response)
+                            }else if(response.other_email.hackerearth == data.email){
+                                resolve(response)
+                            }else{
+                                reject("Please enter the correct email address.")
+                            }
+                        } else {
+                            reject("We regret to inform you that we are unable to proceed with your application to the next round. We wish you all the best for your future endeavors.")
+                    }
+                    }).catch((error) => {
+                        reject(error)
+                    })
+            } else {
+                reject("Please enter a valid Launchpad Kerala ID.")
+            }
+        })
     }
     
 }
--- routes/index.js
+++ routes/index.js
@@ -8,116 +8,155 @@ var app_name = 'Launchpad Kerala'
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  let user = req.user
-  // console.log(req.user);
-  if (user && user.permissions.admin) {
-    res.redirect('/admin/')
-  } else {
-    res.render('index', {
-      title: app_name,
-      home_page: true
-    });
-  }
+    let user = req.user
+    // console.log(req.user);
+    if (user && user.permissions.admin) {
+        res.redirect('/admin/')
+    } else {
+        res.render('index', {
+            title: app_name,
+            home_page: true
+        });
+    }
 });
 
 router.get('/contact', (req, res, next) => {
-  let user = req.user;
-  res.render('pages/contact', {
-    title: `Test Page | ${app_name}`,
-    user
-  });
+    let user = req.user;
+    res.render('pages/contact', {
+        title: `Test Page | ${app_name}`,
+        user
+    });
 });
 
 router.post('/contact', (req, res, next) => {
-  let user = req.user;
-  if (user) {
-    req.body.user = user.id;
-  } else {
-    req.body.user = null;
-  }
-  console.log(req.body)
-  controller.contact.message(req.body)
-    .then((response) => {
-      res.send(
-        {
-          response: "acknowledged",
-          status: true
-        }
-      );
-    })
-    .catch((error) => {
-      res.send(
-        {
-          error,
-          status: false
-        }
-      );
-    })
+    let user = req.user;
+    if (user) {
+        req.body.user = user.id;
+    } else {
+        req.body.user = null;
+    }
+    // console.log(req.body)
+    controller.contact.message(req.body)
+        .then((response) => {
+            res.send(
+                {
+                    response: "acknowledged",
+                    status: true
+                }
+            );
+        })
+        .catch((error) => {
+            res.send(
+                {
+                    error,
+                    status: false
+                }
+            );
+        })
 });
 
 router.get('/updates', function (req, res, next) {
-  res.render('updates',
-    {
-      title: `Updates | ${app_name}`,
-      page_head: 'News & Updates',
-      page_nav_name: 'Updates',
-      breadcrumbs: true,
-      updates_page: true
-    });
+    res.render('updates',
+        {
+            title: `Updates | ${app_name}`,
+            page_head: 'News & Updates',
+            page_nav_name: 'Updates',
+            breadcrumbs: true,
+            updates_page: true
+        });
 });
 
 
 router.get('/privacy-policy', function (req, res, next) {
-  res.render('pages/privacy_policy',
-    {
-      title: `User Pricay Policy | ${app_name}`,
-      page_head: 'User Pricay Policy',
-      page_nav_name: 'pricay policy'
-    });
+    res.render('pages/privacy_policy',
+        {
+            title: `User Pricay Policy | ${app_name}`,
+            page_head: 'User Pricay Policy',
+            page_nav_name: 'pricay policy'
+        });
 });
 
 router.get('/results', function (req, res, next) {
-  res.render('pages/results',
-    {
-      title: `Results | ${app_name}`,
-      page_head: 'Results',
-      page_nav_name: 'Results',
-      reults_page: true
-    });
+    res.render('pages/results',
+        {
+            title: `Results | ${app_name}`,
+            page_head: 'Results',
+            page_nav_name: 'Results',
+            reults_page: true
+        });
 });
 
 router.get('/results/view', function (req, res, next) {
-  let message = req.flash('message');
-  res.render('pages/result',
-    {
-      title: `Results | ${app_name}`,
-      page_head: 'Results',
-      page_nav_name: 'Results',
-      reults_page: true,
-      no_preloader: true,
-      message,
-    });
+    let message = req.flash('message');
+    res.render('pages/result',
+        {
+            title: `Results | ${app_name}`,
+            page_head: 'Results',
+            page_nav_name: 'Results',
+            reults_page: true,
+            no_preloader: true,
+            message,
+        });
 });
 
 router.post('/results/view', function (req, res, next) {
-  // console.log(req.body);
-  controller.candidate.viewResult(req.body)
-    .then((candidate) => {
-      // res.redirect('/results/view');
-      res.render('pages/view_result',
+    controller.candidate.view(req.body)
+        .then((candidate) => {
+            res.render('pages/view_result',
+                {
+                    title: `View Result | ${app_name}`,
+                    page_head: 'Results',
+                    page_nav_name: 'Results',
+                    reults_page: true,
+                    candidate
+                });
+        })
+        .catch((error) => {
+            req.flash('message', error);
+            res.redirect('/results/view');
+        })
+});
+
+router.get('/allotment', function (req, res, next) {
+    res.render('pages/allotment',
         {
-          title: `View Result |${app_name}`,
-          page_head: 'Results',
-          page_nav_name: 'Results',
-          reults_page: true,
-          candidate
+            title: `Allotment | ${app_name}`,
+            page_head: 'Allotment',
+            page_nav_name: 'Allotment',
+            allotment_page: true
         });
-    })
-    .catch((error) => {
-      // console.log(error);
-      req.flash('message', error);
-      res.redirect('/results/view');
-    })
+});
+
+router.get('/allotment/apply', function (req, res, next) {
+    let message = req.flash('message');
+    res.render('pages/allotment/apply',
+        {
+            title: `Allotment Application| ${app_name}`,
+            page_head: 'Allotment',
+            page_nav_name: 'Allotment',
+            allotment_page: true,
+            message
+        });
+});
+
+router.post('/allotment/apply', function (req, res, next) {
+    res.render('pages/allotment/apply',
+        {
+            title: `Allotment Application | ${app_name}`,
+            page_head: 'Allotment',
+            page_nav_name: 'Allotment',
+            allotment_page: true
+        });
+});
+
+router.get('/allotment/recruiters', function (req, res, next) {
+    res.render('pages/allotment/recruiters',
+        {
+            title: `Allotment | ${app_name}`,
+            page_head: 'Allotment',
+            page_nav_name: 'Allotment',
+            allotment_page: true
+        });
 });
 
 module.exports = router;
